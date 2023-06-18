0ae9b90d08adedb0cf505a04ea3d75e6327cdcc8
Dibyansh21 <97114570+Dibyansh21@users.noreply.github.com>
2023-06-18 06:29:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGODB_URI || 'mongodb+srv://user1:user1@cluster0.n17gvsr.mongodb.net/', {
+  .connect(process.env.MONGODB_URI || 'mongodb+srv://user2:user2@cluster0.n17gvsr.mongodb.net/', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
